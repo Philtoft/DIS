6507c2d503836258f7504596c3beb6af61061a98
severincreatives <philip@severincreatives.dk>
2019-09-24 10:51:20 +0200

    ændring i password og brugernavn

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String myUsername = "this is my username";
-        String myPassword = "This is my password";
+        String myUsername = "philtoft";
+        String myPassword = "This is my secret password";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
