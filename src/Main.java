8ad2bf5a0629bf0a65cbb264671bf6c9a0b20ad1
severincreatives <philip@severincreatives.dk>
2019-09-24 10:54:16 +0200

    New username and password

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String myUsername = "philtoft";
-        String myPassword = "This is my secret password";
+        String myUsername = "Philip1234";
+        String myPassword = "Nyt password";
 
         if(!User.validateUsername(myUsername)) {
             System.out.println("Username doesn't fit the rules. Please check the User class.");
